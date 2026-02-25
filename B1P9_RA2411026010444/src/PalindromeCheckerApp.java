7ad0601d61727545d72eb358f7d77f0fef6cbd5b
Neev17964 <neevsharma381@gmail.com>
2026-02-25 11:19:10 +0530

    UC6: Queue + Stack Based Palindrome Check

--- B1P9_RA2411026010444/src/PalindromeCheckerApp.java
+++ B1P9_RA2411026010444/src/PalindromeCheckerApp.java
@@ -1,30 +1,35 @@
 import java.util.Scanner;
 import java.util.Stack;
+import java.util.LinkedList;
+import java.util.Queue;
 
-class UC5_PalindromeCheckerApp {
+class UC6_PalindromeCheckerApp {
 
     public static void main(String[] args) {
-        System.out.println("--- UC5: Stack-Based Palindrome Checker ---");
+        System.out.println("--- UC6: Queue + Stack Based Palindrome Check ---");
 
         Scanner scanner = new Scanner(System.in);
         System.out.print("Input Text: ");
         String word = scanner.nextLine();
 
+        Queue<Character> queue = new LinkedList<>();
         Stack<Character> stack = new Stack<>();
 
         for (int i = 0; i < word.length(); i++) {
+            queue.offer(word.charAt(i));
             stack.push(word.charAt(i));
         }
 
-        String stackReversed = "";
-        while (!stack.isEmpty()) {
-            stackReversed = stackReversed + stack.pop();
+        boolean isPalindrome = true;
+
+        while (!queue.isEmpty() && !stack.isEmpty()) {
+            if (queue.poll() != stack.pop()) {
+                isPalindrome = false;
+                break;
+            }
         }
 
-        boolean isPalindrome = word.equals(stackReversed);
-
         System.out.println("Input Text          : " + word);
-        System.out.println("Reversed Text       : " + stackReversed);
         System.out.println("Is it a Palindrome? : " + isPalindrome);
 
         scanner.close();
